3c5ea30a716793e42ed200d7a5d97cb434a88b3e
Damian Costa <damiancb97@gmail.com>
2024-10-16 13:53:52 +0200

    Tarea 1.2 apartado 6

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -1,4 +1,4 @@
-<html>
+<html
 <body>
 <h1>Conversor de longitudes</h1>
 <p>Convierte de la unidad especificada a metros</p>
@@ -9,6 +9,10 @@ if ($_POST["funidad"] == "pulgada") {
 $v_pulgadas = $_POST["fcantidad"];
 $v_metros = $v_pulgadas * 0.0254;
 echo $v_pulgadas."pulgada(s) = ".$v_metros." metro(s)";
+}elseif($_POST["funidad"] == "pies"){
+$v_pies = $_POST["fcantidad"];
+$v_metros = $v_pies * 0.3048;
+echo $v_pies."pies = ".$v_metros." metro (s)";
 } else {
 echo "Unidad no soportada";
 }
@@ -21,6 +25,8 @@ echo "Unidad no soportada";
 <input type="text" id="cantidad_input" name="fcantidad"><br>
 <input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 <label for="pulgada_input">Pulgada(s)</label><br>
+<input type="radio" id="pies_input" name="funidad" value="pies">
+<label for="pies_input">Pies(ft- sistema imperial)</label><br>
 <input type="radio" id="otro_input" name="funidad" value="otro">
 <label for="otro_input">Otro</label><br>
 <input type="submit" value="Convertir">
